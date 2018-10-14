55b9877d5a4f9339bcb330ab187ba8e4ee479034
Tanya Lipnitskaya <fenya-ne@yandex.ru>
2018-10-14 19:11:41 +0700

    Geolocation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const temp = document.getElementById("temp")
 // запрос данных с API, преобразоване полученных данных в JSON
-fetch("http://api.openweathermap.org/data/2.5/weather?q=NOVOSIBIRSK&units=metric&appid=fa36f6e3bd7d694e6b8d6b9bfedc8221").then((value) => value.json()).then((value) => temp.innerHTML = value.main.temp)
+const activeWeather = document.getElementById("activeWeather")
+const icon = document.getElementById("icon")
+const wind = document.getElementById("wind")
+const city = document.getElementById("city")
+
+// Запрос геолокации
+navigator.geolocation.getCurrentPosition(
+    function (position) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=fa36f6e3bd7d694e6b8d6b9bfedc8221&lang=ru`).then((value) => value.json()).then((response) => {
+            temp.innerHTML = response.main.temp
+            activeWeather.innerHTML = response.weather[0].description
+            icon.src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+            wind.innerHTML = response.wind.speed
+            city.innerHTML = response.name
+        })
+    },
+    function (error) {
+        document.body.innerHTML = "Нет геопозиции - нет погоды, жадина"
+    }
+
+)
