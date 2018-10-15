6ed0299a15a6358a76c4cae85bf9e8e06dfc96ec
Konstantin <konstantin@MAC12.local>
2018-10-15 14:56:14 +0700

    Create config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+
+const APPID = 'fa36f6e3bd7d694e6b8d6b9bfedc8221';
+
+module.exports = { APPID };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { APPID } = require('./config');
+
 const temp = document.getElementById("temp")
 // запрос данных с API, преобразоване полученных данных в JSON
 const activeWeather = document.getElementById("activeWeather")
@@ -5,10 +7,11 @@ const icon = document.getElementById("icon")
 const wind = document.getElementById("wind")
 const city = document.getElementById("city")
 
+
 // Запрос геолокации
 navigator.geolocation.getCurrentPosition(
     function (position) {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=fa36f6e3bd7d694e6b8d6b9bfedc8221&lang=ru`).then((value) => value.json()).then((response) => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${APPID}&lang=ru`).then((value) => value.json()).then((response) => {
             temp.innerHTML = response.main.temp
             activeWeather.innerHTML = response.weather[0].description
             icon.src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
