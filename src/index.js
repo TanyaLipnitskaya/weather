bd7905ba5dca0323dfc3d611657c1fbdad4a304d
Tanya Lipnitskaya <fenya-ne@yandex.ru>
2018-10-17 21:33:41 +0700

    3 days forecast(!)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const { APPID } = require('./config');
+const {
+    APPID
+} = require('./config');
 
 const temp = document.getElementById("temp")
 // запрос данных с API, преобразоване полученных данных в JSON
@@ -6,21 +8,34 @@ const activeWeather = document.getElementById("activeWeather")
 const icon = document.getElementById("icon")
 const wind = document.getElementById("wind")
 const city = document.getElementById("city")
+const today = document.getElementsByClassName("today")[0]
+const tomorrow = document.getElementsByClassName("tomorrow")[0]
+const theDayAfterTomorrow = document.getElementsByClassName("theDayAfterTomorrow")[0]
+
+function dayDefiner(day) {
+    // Запрос геолокации
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${APPID}&lang=ru`).then((value) => value.json()).then((responseList) => {
+                const response = responseList.list[day]
+                temp.innerHTML = response.temp.day
+                activeWeather.innerHTML = response.weather[0].description
+                icon.src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+                wind.innerHTML = response.speed
+                city.innerHTML = responseList.city.name
+            })
+        },
+        function (error) {
+            document.body.innerHTML = "Нет геопозиции - нет погоды, жадина"
+        }
+
+    )
 
 
-// Запрос геолокации
-navigator.geolocation.getCurrentPosition(
-    function (position) {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${APPID}&lang=ru`).then((value) => value.json()).then((response) => {
-            temp.innerHTML = response.main.temp
-            activeWeather.innerHTML = response.weather[0].description
-            icon.src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
-            wind.innerHTML = response.wind.speed
-            city.innerHTML = response.name
-        })
-    },
-    function (error) {
-        document.body.innerHTML = "Нет геопозиции - нет погоды, жадина"
-    }
 
-)
+}
+dayDefiner(0)
+
+today.addEventListener("click", () => dayDefiner(0))
+tomorrow.addEventListener("click", () => dayDefiner(1))
+theDayAfterTomorrow.addEventListener("click", () => dayDefiner(2))
