b4bca1e172289f610e379a5d0d7d52c14dab51eb
Tanya Lipnitskaya <fenya-ne@yandex.ru>
2018-10-20 19:27:02 +0700

    Background image

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const city = document.getElementById("city")
 const today = document.getElementsByClassName("today")[0]
 const tomorrow = document.getElementsByClassName("tomorrow")[0]
 const theDayAfterTomorrow = document.getElementsByClassName("theDayAfterTomorrow")[0]
+const app = document.getElementsByClassName("container")[0]
 
 function dayDefiner(day) {
     // Запрос геолокации
@@ -23,6 +24,13 @@ function dayDefiner(day) {
                 icon.src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
                 wind.innerHTML = response.speed
                 city.innerHTML = responseList.city.name
+                // list[""0""].weather[""0""].main
+                fetch(`https://pixabay.com/api/?key=10451328-bd146c8c607b04a23ec6a233c&category=nature&pretty=true&q=${response.weather[0].main}`).then((value) => value.json()).then((pictureList) => {
+                    const pictureURL = pictureList.hits[0].largeImageURL
+                    app.style.backgroundImage = `url(${pictureURL})`
+
+                })
+
             })
         },
         function (error) {
@@ -34,8 +42,25 @@ function dayDefiner(day) {
 
 
 }
+// обработчик события
+function makeButtonActive(event) {
+    const currentButton = event.target;
+    const previouslyActive = document.querySelector("button.active");
+    previouslyActive.classList.remove("active")
+    currentButton.classList.add("active")
+
+}
 dayDefiner(0)
 
-today.addEventListener("click", () => dayDefiner(0))
-tomorrow.addEventListener("click", () => dayDefiner(1))
-theDayAfterTomorrow.addEventListener("click", () => dayDefiner(2))
+today.addEventListener("click", (event) => {
+    dayDefiner(0);
+    makeButtonActive(event)
+})
+tomorrow.addEventListener("click", (event) => {
+    dayDefiner(1);
+    makeButtonActive(event)
+})
+theDayAfterTomorrow.addEventListener("click", (event) => {
+    dayDefiner(2);
+    makeButtonActive(event)
+})
